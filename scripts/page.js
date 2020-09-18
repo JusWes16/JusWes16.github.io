572c836bd22eebcbb4666edcd99628b95d772692
westonk@byui.edu <71291546+JusWes16@users.noreply.github.com>
2020-09-18 14:52:07 -0600

    "fixed last 'last updated' code"

--- scripts/page.js
+++ scripts/page.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', (event)=>{
     const lu = document.querySelector('#lastupdated');
-    lu.textContext = document.lastModified;
+    lu.textContent = document.lastModified;
 
     const cry = document.querySelector('#copyrightyear');
     cry.textContent = new Date().getFullYear();
