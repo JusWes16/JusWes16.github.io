d20ed5924923c7f6f29a79a248c0bd8b0ec85ff5
westonk@byui.edu <71291546+JusWes16@users.noreply.github.com>
2020-10-19 16:14:58 -0600

    preston 5

--- lesson5/js/preston-5.js
+++ lesson5/js/preston-5.js
@@ -1,12 +1,14 @@
  window.addEventListener('load', (event)=>{
-    const lu = document.querySelector('#lastupdated');
-    lu.textContent = new Date()
+    const cd = document.querySelector('#current-date');
+    let day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novemeber', 'December']
+    cd.textContent = day[new Date().getDay()] + ', ' + new Date().getDate() + ' ' + month[new Date().getMonth()] + ' ' + new Date().getFullYear();
 
     const cry = document.querySelector('#copyrightyear');
     cry.textContent = new Date().getFullYear();
 
     var nb = document.querySelector('.notification')
-    var d = new Date().getDay()
+    var d = new Date().getDay();
 
     if (d == 5) {
         nb.style.display = 'block'
