9d97be122769cb49bfa1a6b4a37ad1cd93a7e4ea
westonk@byui.edu <71291546+JusWes16@users.noreply.github.com>
2020-10-19 16:32:22 -0600

    preston 5 final

--- lesson5/js/preston-5.js
+++ lesson5/js/preston-5.js
@@ -1,7 +1,28 @@
  window.addEventListener('load', (event)=>{
     const cd = document.querySelector('#current-date');
-    let day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novemeber', 'December']
+    let day = [
+        'Sunday', 
+        'Monday', 
+        'Tuesday', 
+        'Wednesday', 
+        'Thursday', 
+        'Friday', 
+        'Saturday'
+    ]
+    let month = [
+        'January', 
+        'February', 
+        'March', 
+        'April', 
+        'May', 
+        'June', 
+        'July', 
+        'August', 
+        'September', 
+        'October', 
+        'Novemeber', 
+        'December'
+    ]
     cd.textContent = day[new Date().getDay()] + ', ' + new Date().getDate() + ' ' + month[new Date().getMonth()] + ' ' + new Date().getFullYear();
 
     const cry = document.querySelector('#copyrightyear');
