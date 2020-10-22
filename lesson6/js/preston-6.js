e07ce6dd7ffbe30f45bb6c7236875caeaa8444ba
westonk@byui.edu <71291546+JusWes16@users.noreply.github.com>
2020-10-21 23:37:29 -0600

    preston 6

--- lesson6/js/preston-6.js
+++ lesson6/js/preston-6.js
@@ -91,6 +91,14 @@ window.addEventListener('load', (event)=>{
     } else if (new Date().getDay() == 6){
         day_5.textContent = curDay[new Date().getDay() - 3];
     }*/
+
+//Code to calculate wind chill temp 
+    var temp = document.getElementById("temp-today").innerHTML;
+    var wind = document.getElementById("wind-speed").innerHTML;
+    var windChill = document.getElementById("wind-chill");
+
+    var chill = 35.74 + (0.6215 * temp) - (35.75 * (Math.pow(wind, 0.16))) + (0.4275 * temp * (Math.pow(wind, 0.16)));
+    windChill.textContent = Math.round(chill, 0);
     
 })
 
