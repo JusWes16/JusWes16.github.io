913235796a471c9e1535cd48aeb1f41cab422be9
westonk@byui.edu <71291546+JusWes16@users.noreply.github.com>
2020-10-23 00:04:39 -0600

    preston

--- lesson6/js/preston-6.js
+++ lesson6/js/preston-6.js
@@ -107,8 +107,6 @@ $(function() {
 		// checks if its the same on the address bar
 		if (url == (this.href)) {
 			$(this).closest("li").addClass("active");
-			//for making parent of submenu active
-			$(this).closest("li").parent().parent().addClass("active");
 		}
 	});
 });
