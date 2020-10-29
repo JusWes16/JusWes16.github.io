2415cd728016216d4298553a3f94ff24435ac870
westonk@byui.edu <71291546+JusWes16@users.noreply.github.com>
2020-10-28 23:12:50 -0600

    clean up

--- ICE/ICE Gallery/gallery.js
+++ ICE/ICE Gallery/gallery.js
@@ -1,7 +1,6 @@
 window.addEventListener('load', (event) => {
     var modal = document.getElementById("myModal");
 
-    // Get the image and insert it inside the modal - use its "alt" text as a caption
     var img = document.getElementsByClassName("image");
     var modalImg = document.getElementById("img01");
     var captionText = document.getElementById("caption");
@@ -12,10 +11,8 @@ window.addEventListener('load', (event) => {
         captionText.innerHTML = this.alt;
         }
     }
-    // Get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
 
-    // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
     modal.style.display = "none";
     }
