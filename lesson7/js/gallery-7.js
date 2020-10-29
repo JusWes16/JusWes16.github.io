4d40f5f45e3051265b6cc72025bd12c433eba327
westonk@byui.edu <71291546+JusWes16@users.noreply.github.com>
2020-10-29 01:52:17 -0600

    gallery

---
+++ lesson7/js/gallery-7.js
@@ -0,0 +1,61 @@
+window.addEventListener('load', (event) => {
+    
+    //Returns the current date at the bottom in the format "Monday, 12 October 2020"
+	const cd = document.querySelector('#current-date');
+	let day = [
+		'Sunday',
+		'Monday',
+		'Tuesday',
+		'Wednesday',
+		'Thursday',
+		'Friday',
+		'Saturday'
+	];
+	let month = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'Novemeber',
+		'December'
+	];
+    cd.textContent = day[new Date().getDay()] + ', ' + new Date().getDate() + ' ' + month[new Date().getMonth()] + ' ' + new Date().getFullYear();
+    
+    //Returns current year to the copyright in the footer
+	const cry = document.querySelector('#copyrightyear');
+    cry.textContent = new Date().getFullYear();
+    
+    //Function to toggle hamburger button
+    function menu() {
+	    document.getElementById("navi").classList.toggle("responsive");
+    }
+
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementsByClassName("images");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    for (i = 0; i < img.length; i++) {
+        img[i].onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+        captionText.innerHTML = this.alt;
+        }
+    }
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+    modal.style.display = "none";
+    }
+});
